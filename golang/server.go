2292539a7cbd3a262bb0af700453e73849592f7a
skxeve <skxeve@gmail.com>
2019-02-12 18:39:04 +0900

    auth

--- golang/server.go
+++ golang/server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	firebase "firebase.google.com/go"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
 	"net/http"
 )
 
@@ -42,9 +45,43 @@ func viewHandler2(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 }
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	opt := option.WithCredentialsFile("secrets/try-firebase-auth_adminsdk.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+
+	html := HtmlTemplates{
+		Dir:     "templates/",
+		Layout:  "layout.tmpl",
+		Content: "content.tmpl",
+	}
+
+	client, err := app.Auth()
+	if err != nil {
+		page := Page{
+			Title:  "Error auth",
+			Count:  1,
+			Header: err.Error(),
+		}
+		if err := html.Execute(w, page); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	page := Page{
+		Title:  "Hello Golang!",
+		Count:  2,
+		Header: "Hoge",
+	}
+
+	if err := html.Execute(w, page); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	http.HandleFunc("/", viewHandler)
 	http.HandleFunc("/next", viewHandler2)
+	http.HandleFunc("/auth", authHandler)
 	http.ListenAndServe(":8080", nil)
 }
